6b9118488235fc5b1426b054d42906da307b7c13
edwardconnect <elchu@connect.ust.hk>
2020-02-03 16:49:27 +0800

    Init - travel form guiderootJS

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import stylesheets
 import './style.css';
+import $ from "jquery";
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+$('#app').val('a')
